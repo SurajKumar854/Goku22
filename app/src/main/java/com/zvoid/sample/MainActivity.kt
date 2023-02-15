8b31ad47b965699e8383fb03051fa0cbe08e30d2
Suraj Kumar <ersuraj854@gmail.com>
2023-02-15 11:28:19 +0530

    Surajh

---
+++ app/src/main/java/com/zvoid/sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zvoid.sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
